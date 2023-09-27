44a07484a419c781fc4c04c26a26052297a17ff5
Dogura <76627232+Dogura@users.noreply.github.com>
2023-09-27 17:06:04 +0200

    Ready to deploy

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password : "bubik",
-    database : "mobile_app",
+    host: "bk17dasluoctd9vhyo5m-mysql.services.clever-cloud.com",
+    user: "uv7it9v85zrpfria",
+    password : "WKi42mADpIcruRnJ724L",
+    database : "bk17dasluoctd9vhyo5m",
     port: 3306 
 });
