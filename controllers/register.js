b511da42a27a3f66b5241db6b8a2115c161e4f46
Dogura <76627232+Dogura@users.noreply.github.com>
2023-10-21 13:23:36 +0200

    Ready to deploy

--- controllers/register.js
+++ controllers/register.js
@@ -10,19 +10,25 @@ export const register = (req,res)=>{
     const q = "SELECT * FROM users WHERE userName = ?"
     db.query(q,[req.body.username],(err,data)=>{
         if(err){
+            console.log("error 1st")
             return res.json(err)
         }
+        console.log("register in middle of 1stquery " +data.length )
         if (data.length != 0) return res.status(404).json("UserName taken");
     });
 
     const pass = bcrypt.hashSync(req.body.password, 10);
+    console.log("password hashed")
 
     const ins = "INSERT INTO users ( FirstName, Surname, Privileges, Password, userName) VALUES (?, ?, ?, ?, ?)"
     db.query(ins,[req.body.name,req.body.surrname,0,pass,req.body.username],(err,data)=>{
         if(err){
+            console.log("2nd error")
             return res.json(err)
         }
-       else return res.status(200).json("registered");
+       else {
+        console.log("ez put in to database")
+        return res.status(200).json("registered");};
     });
     
 };
