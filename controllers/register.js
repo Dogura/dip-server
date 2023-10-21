73ed3d451b6d6a533e94ccc0576994e7f583785d
Dogura <76627232+Dogura@users.noreply.github.com>
2023-10-21 13:10:32 +0200

    Ready to deploy

--- controllers/register.js
+++ controllers/register.js
@@ -5,26 +5,24 @@ import jwt from "jsonwebtoken";
 export const register = (req,res)=>{
 
     console.log("register from phone");
-    return res.status(404).json("User not found!")
-    /*
+   
+    
     const q = "SELECT * FROM users WHERE userName = ?"
     db.query(q,[req.body.username],(err,data)=>{
         if(err){
             return res.json(err)
         }
-        if (data.length === 0) return res.status(404).json("User not found!");
-        
-        const isPassCorrect = bcrypt.compareSync(req.body.password, data[0].Password)
-        if(!isPassCorrect) return res.status(400).json("wrong username or password!")
-
-        const {Password, ...other} = data[0] 
-
-        const token = jwt.sign({id:data[0].idusers},"jwtkey");
-        res.cookie("access_token",token,{
-            httpOnly:true
-        }).status(200).json(other)
+        if (data.length != 0) return res.status(404).json("UserName taken");
     });
-    */
 
+    const pass = bcrypt.hashSync(req.body.password, 10);
 
+    const ins = "INSERT INTO users ( FirstName, Surname, Privileges, Password, userName) VALUES (?, ?, ?, ?, ?)"
+    db.query(ins,[req.body.name,req.body.surrname,0,pass,req.body.username],(err,data)=>{
+        if(err){
+            return res.json(err)
+        }
+       else return res.status(200).json("registered");
+    });
+    
 };
