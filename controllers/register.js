79d2e1894f4905f0fe953351815f5bfe5090ef4a
Dogura <76627232+Dogura@users.noreply.github.com>
2023-11-03 00:16:12 +0100

    Ready to deploy

--- controllers/register.js
+++ controllers/register.js
@@ -8,7 +8,14 @@ export const register = async (req, res) => {
     const q = "SELECT * FROM users WHERE userName = ?";
 
     try {
-        const data = await db.query(q, [req.body.username])
+        const data = await  new Promise((resolve, reject) => {
+            db.query(q, [req.body.username], (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }});
+        });
 
         console.log("register in middle of 1st query " + data.length);
 
