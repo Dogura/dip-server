3787b49f2c7ad6fda7ceb2caf19cb995057edc4f
Dogura <76627232+Dogura@users.noreply.github.com>
2023-11-02 20:16:16 +0100

    Ready to deploy

--- controllers/register.js
+++ controllers/register.js
@@ -2,43 +2,23 @@ import {db} from "../db.js"
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-export  const register =  (req,res)=>{
-
+export const register = async (req, res) => {
     console.log("register from phone");
-   
-    
-    const q = "SELECT * FROM users WHERE userName = ?"
-     db.query(q,[req.body.username],  (err,data)=>{
-        if(err){
-            console.log("error 1st")
-            return res.json(err)
-        }
-        console.log("register in middle of 1stquery " +data.length )
+
+    const q = "SELECT * FROM users WHERE userName = ?";
+
+    try {
+        const data = await db.query(q, [req.body.username]);
+
+        console.log("register in middle of 1st query " + data.length);
+
         if (data.length !== 0) {
             return res.status(409).json("UserName taken");
-        }else {
-            return res.status(200).json("succes")
-            /*
-            const pass = bcrypt.hashSync(req.body.password, 10);
-            console.log("password hashed")
-
-            const ins = "INSERT INTO users ( FirstName, Surname, Privileges, Password, userName) VALUES (?, ?, ?, ?, ?)"
-            return  db.query(ins,[req.body.name,req.body.surrname,0,pass,req.body.username],(err,data)=>{
-                if(err){
-                    console.log("2nd error")
-                    return res.status(422).json(err)
-                }
-                else {
-                    console.log("ez put in to database")
-                    return res.status(200).json("succes");
-
-                };
-            });*/
-            
-       
+        } else {
+            return res.status(200).json("success");
         }
-        
-    });
-
-    
+    } catch (err) {
+        console.log("Error:", err);
+        return res.status(500).json(err);
+    }
 };
