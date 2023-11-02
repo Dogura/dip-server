5a1ba9425c473684113b528f842a8360d0f4b67e
Dogura <76627232+Dogura@users.noreply.github.com>
2023-11-02 20:34:06 +0100

    Ready to deploy

--- controllers/register.js
+++ controllers/register.js
@@ -16,10 +16,13 @@ export const register = async (req, res) => {
             console.log(data[0])
             return res.status(409).json("UserName taken");
         } else {
+            console.log("Success in database")
             return res.status(200).json("success");
         }
     } catch (err) {
         console.log("Error:", err);
         return res.status(500).json(err);
     }
+
+    console.log("somehow im out")
 };
