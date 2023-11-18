41e4e3b6057dd3904ccc2319980c9d8d7e703d89
Dogura <76627232+Dogura@users.noreply.github.com>
2023-11-18 19:20:15 +0100

    Ready to deploy

--- controllers/login.js
+++ controllers/login.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 
 
 export const login = (req,res)=>{
-    console.log("login comming from phone" + JSON.stringify(req.body));
     const q = "SELECT * FROM users WHERE userName = ?"
     db.query(q,[req.body.username],(err,data)=>{
         console.log("data are " + data);
