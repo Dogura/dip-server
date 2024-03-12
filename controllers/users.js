5a75f2719151e06c315b97a75053c421dfa5b416
Dogura <76627232+Dogura@users.noreply.github.com>
2024-03-12 17:21:46 +0100

    handel async

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,13 @@
 import {database, db} from "../db.js"
 import { getDatabase, ref, child, get, set } from "firebase/database";
 
-export const getUsers = (req,res)=>{
+export const getUsers = async (req,res)=>{
     var priv = req.body.priviledges
     var id = req.body.key
     var list =[]
     const dbRef = ref(database)
 
-    get(child(dbRef,"Groupchat/Users")).then((snapshot) => {    
+    await get(child(dbRef,"Groupchat/Users")).then((snapshot) => {    
         if (snapshot.exists()) {
             snapshot.forEach(element => {
             if((id != element.val().key) && (priv >= element.val().priviledges) ){
@@ -27,11 +27,11 @@ export const getUsers = (req,res)=>{
     });
 };
 
-export const setUsers = (req,res)=>{
+export const setUsers = async (req,res)=>{
     var priv = req.body.priv
     var id = req.body.id
     const dbRef = database
-    set(ref(database,'Groupchat/Users/'+id+'/priviledges'),priv)
+    await set(ref(database,'Groupchat/Users/'+id+'/priviledges'),priv)
     .then(() => {
       console.log('Value set successfully');
       return res.status(200)
