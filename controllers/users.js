1684343a524b63203754e78ada27c0576d3942e4
Dogura <76627232+Dogura@users.noreply.github.com>
2024-04-12 20:58:50 +0200

    pass reset

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 import {database, db} from "../db.js"
+import bcrypt from "bcryptjs";
 import { getDatabase, ref, child, get, set } from "firebase/database";
 
 export const getUsers = async (req,res)=>{
@@ -36,6 +37,30 @@ export const setUsers = async (req,res)=>{
 
         console.log("Going into set2");
         await set(ref(database, 'Groupchat/Users/' + id + '/priviledges'), priv);
+
+        console.log("Value set successfully");
+        return res.status(200).send("Value set successfully");
+    } catch (error) {
+        console.error('Error setting value:', error);
+        return res.status(500).send("Error setting value: " + error);
+    }
+
+};
+export const resetPass = async (req,res)=>{
+    const saltRounds = 10; // You can adjust the number of salt rounds as needed
+    const plaintextPassword = 'Heslo123';
+    console.log("Going into reset ");
+    var hh;
+    hh = await bcrypt.hash(plaintextPassword, saltRounds);
+
+    console.log("After bcrypt.hash()",hh);
+    console.log("Going into set12 ",hh);
+    try {
+        const priv = req.body.priv;
+        const id = req.body.id;
+
+        console.log("Going into set2 ",hh);
+        await set(ref(database, 'Groupchat/Users/' + id + '/password'), hh);
 
         console.log("Value set successfully");
         return res.status(200).send("Value set successfully");
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import  express  from "express";
-import { getUsers, setUsers } from "../controllers/users.js";
+import { getUsers, setUsers,resetPass } from "../controllers/users.js";
 
 const router = express.Router()
 
 router.post("/getUsers",getUsers)
 router.post("/setUsers",setUsers)
+router.post("/resetPass",resetPass)
 
 export default router
